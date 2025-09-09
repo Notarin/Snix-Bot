942c4a708d1cf96bf550446acc2d19841932def7
Notarin Steele <424c414e4b@gmail.com>
2025-09-09 13:27:06 -0400

    Set up reusing existing nixpkgs repo

--- src/nixpkgs.rs
+++ src/nixpkgs.rs
@@ -2,33 +2,35 @@ use crate::args::ARGS;
 use git2::build::RepoBuilder;
 use git2::{FetchOptions, Repository};
 use lazy_static::lazy_static;
-use log::{debug, info};
-use tempfile::{TempDir, tempdir};
+use log::info;
+use std::path::PathBuf;
+use tempfile::env::temp_dir;
 use tokio::sync::Mutex;
 
 lazy_static! {
-    static ref NixpkgsDir: TempDir = {
-        let dir: TempDir = tempdir().expect("Failed to create dir to place nixpkgs in!");
-        debug!(
-            "Temp path for nixpkgs repo chosen: {}",
-            dir.path().display()
-        );
-        dir
-    };
-    pub(crate) static ref NixpkgsPath: &'static str = {
-        NixpkgsDir
-            .path()
-            .to_str()
-            .expect("For some bizarre reason, the tempdir for nixpkgs was not UTF-8")
-    };
-    pub(crate) static ref NixpkgsRepo: Mutex<Repository> = Mutex::new(clone_nixpkgs());
+    pub(crate) static ref NixpkgsPath: PathBuf = temp_dir().join(PathBuf::from("nixpkgs"));
+    pub(crate) static ref NixpkgsRepo: Mutex<Repository> = Mutex::new(nixpkgs_repo());
+}
+
+pub(crate) fn nixpkgs_repo() -> Repository {
+    info!("Getting nixpkgs repo");
+    match Repository::open(&*NixpkgsPath) {
+        Ok(repository) => {
+            info!("nixpkgs is already cloned! Using that.");
+            repository
+        }
+        Err(_) => {
+            info!("nixpkgs was not already cloned, cloning it.");
+            clone_nixpkgs()
+        }
+    }
 }
 
 pub fn clone_nixpkgs() -> Repository {
     info!("Starting nixpkgs clone!");
     RepoBuilder::new()
         .fetch_options(clone_options())
-        .clone(&ARGS.nixpkgs_url, NixpkgsDir.path())
+        .clone(&ARGS.nixpkgs_url, &NixpkgsPath)
         .expect("Failed to clone nixpkgs!")
 }
 
