f95a7956c395db8361333416076d7e6b6142b440
Notarin Steele <424c414e4b@gmail.com>
2025-09-12 11:21:29 -0400

    Added timeout to snix eval

--- src/commands.rs
+++ src/commands.rs
@@ -139,49 +139,64 @@ async fn eval_discord_expression(
     expression: String,
 ) -> Result<Result<(), Error>, Error> {
     let response: String = {
-        let output: Result<String, Error> = tokio::task::spawn_blocking(move || {
-            let mode = snix_eval::EvalMode::Strict;
-            // Making our fake derivation builder
-            let fake_derivation_builder = Evaluation::evaluate(
-                snix_eval::Evaluation::builder_pure()
-                    .mode(mode)
-                    .enable_import()
-                    .enable_impure(Some(Box::new(NixpkgsIo)))
-                    .build(),
-                // Do the absolue least possible to get by
-                "arg: arg // {out={type=null;outputName=null;};}",
-                Some(NixpkgsPath.as_path().into()),
-            )
-            .value
-            .unwrap();
-            // Making our fake placeholder
-            let fake_placeholder = Evaluation::evaluate(
-                snix_eval::Evaluation::builder_pure()
-                    .mode(mode)
-                    .enable_import()
-                    .enable_impure(Some(Box::new(NixpkgsIo)))
-                    .build(),
-                // Do fuckall with any arguments
-                "arg: arg",
-                Some(NixpkgsPath.as_path().into()),
-            )
-            .value
-            .unwrap();
+        use tokio::time::{Duration, timeout};
 
-            let builder = snix_eval::Evaluation::builder_pure()
-                .mode(mode)
-                .enable_import()
-                .add_builtins(vec![
-                    ("derivation", fake_derivation_builder),
-                    ("placeholder", fake_placeholder),
-                ])
-                .enable_impure(Some(Box::new(NixpkgsIo)));
-            let evaluation = builder.build();
-            let result: EvaluationResult =
-                Evaluation::evaluate(evaluation, expression, Some(NixpkgsPath.as_path().into()));
-            Ok(format!("{}", check_value_for_errors(result)?).clone())
-        })
+        let eval_timeout: Duration = Duration::from_secs(2);
+        let output: Result<String, Error> = timeout(
+            eval_timeout,
+            tokio::task::spawn_blocking(move || {
+                let mode = snix_eval::EvalMode::Strict;
+
+                let fake_derivation_builder = Evaluation::evaluate(
+                    snix_eval::Evaluation::builder_pure()
+                        .mode(mode)
+                        .enable_import()
+                        .enable_impure(Some(Box::new(NixpkgsIo)))
+                        .build(),
+                    "arg: arg // {out={type=null;outputName=null;};}",
+                    Some(NixpkgsPath.as_path().into()),
+                )
+                .value
+                .unwrap();
+
+                let fake_placeholder = Evaluation::evaluate(
+                    snix_eval::Evaluation::builder_pure()
+                        .mode(mode)
+                        .enable_import()
+                        .enable_impure(Some(Box::new(NixpkgsIo)))
+                        .build(),
+                    "arg: arg",
+                    Some(NixpkgsPath.as_path().into()),
+                )
+                .value
+                .unwrap();
+
+                let builder = snix_eval::Evaluation::builder_pure()
+                    .mode(mode)
+                    .enable_import()
+                    .add_builtins(vec![
+                        ("derivation", fake_derivation_builder),
+                        ("placeholder", fake_placeholder),
+                    ])
+                    .enable_impure(Some(Box::new(NixpkgsIo)));
+
+                let evaluation = builder.build();
+                let result: EvaluationResult = Evaluation::evaluate(
+                    evaluation,
+                    expression,
+                    Some(NixpkgsPath.as_path().into()),
+                );
+
+                Ok(format!("{}", check_value_for_errors(result)?))
+            }),
+        )
         .await
+        .map_err(|_| {
+            format!(
+                "Evaluation took too long. Max eval time is {} seconds.",
+                eval_timeout.as_secs()
+            )
+        })?
         .unwrap();
         output?
     };
